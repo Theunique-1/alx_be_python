365569c83f25060464cf29642e8df26de6f15727
Theunique-1 <udopeter882@gmail.com>
2025-01-19 14:22:50 -0800

    updated file operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,14 @@
 def perform_operations(num1, num2, operation):
-# to perform basic oprtion
-    if operation == 'add':
-        return num1 + num2
-    elif operation == 'subtract':
-        return(num1 - num2)
-    elif operation == 'multiply':
-        return num1 * num2
-    elif operation == 'divide':
-        if num2 == 0:
-            return "Error: Cannot divide by zero"
+if operation == 'add':
+    return num1 + num2
+elif operation == 'subtract':
+    return(num1 - num2)
+elif operation == 'multiply':
+    return num1 * num2
+elif operation == 'divide':
+    if num2 == 0:
+        return "Error: Cannot divide by zero"
         else:
          return num1 / num2
-    else:
-        return "Invalid operation"
+else:
+    return "Invalid operation"
