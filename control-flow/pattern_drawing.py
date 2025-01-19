745a10f78b943791e85f1345235de561b4006240
Theunique-1 <udopeter882@gmail.com>
2025-01-19 09:08:21 -0800

    File pattern_drawing.py has been updated

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,11 +1,9 @@
-pattern_size = 0
-while pattern_size <= 0:
-    pattern_size = int("Enter the size of the pattern: ")
-    i = 0
-    while i < pattern_size:
-        j = 0
-        while j < pattern_size:
-            print("*", end="")
-            j += 1
-        print()
-        i += 1
+pattern_size = int(input("Enter the size of the pattern: "))
+i = 0
+while i < pattern_size:
+    j = 0
+    while j < pattern_size:
+        print("*", end="")
+        j += 1
+    print()
+    i += 1
